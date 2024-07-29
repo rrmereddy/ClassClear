4a0b1cf7b61cb90db4243528d3bde3aa4bb0ce35
TG30 <ritinmeredy@gmail.com>
2024-07-29 16:20:17 -0500

    Resolved Merge Conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,21 +34,11 @@ export default{
             transform: 'rotate(360deg)',
           },
         },
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         'border-spin': 'border-spin 5s linear infinite',
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
